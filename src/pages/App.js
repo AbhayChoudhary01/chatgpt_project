29de57b345ef1f3fd1260b991938ea3785d59f9c
Abhay Choudhary <abhay@Abhays-MacBook-Air.local>
2023-02-14 23:21:14 -0500

    css styling

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,12 +1,18 @@
+import React from 'react';
+
 import Navbar from "../components/Navbar"
 import BottomBox from "../components/BottomBox"
 import LeftBox from "../components/LeftBox"
-import RightBox from '../components/RightBox';
-import '../css/App.css';
+import RightBox from '../components/RightBox'
+
+import '../css/App.css'
 
 export default function App() {
 
-
+  const[currentForm, setCurrentForm] = React.useState('login')
+  function toggleForm(){
+      setCurrentForm()
+  }
   return (
     <div className="login-page-body">
 
