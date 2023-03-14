1a5c285ae407450b4939a75c2da24f7506e60bf4
Abhay <abhaychoudhary804@gmail.com>
2023-03-14 19:52:09 -0400

    apis for login, signup and user schema prepared

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,24 +1,61 @@
 const router = require('express').Router();
 let User = require('../models/user.model');
+const axios = require('axios');
 
-router.route('/').get((req, res) => {
-  console.log("Users get request");
-  User.find()
-    .then(users => res.json(users))
-    .catch(err => res.status(400).json('Error: ' + err));
-});
+//LOGIN API
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  console.log(req.body);
 
-router.route('/add').post((req, res) => {
+  try {
+    const user = await User.findOne({ username, password });
+    console.log("done");
+    if (user) {
+      console.log("connecting mere bhai");
+      res.status(200).send({ message: 'Login successful!' });
+      console.log("ho gya mere bhai");
 
-  console.log("request for adding : ");
-  console.log(req.body.username);
-  const username = req.body.username;
+    } else {
+      res.status(401).send({ message: 'Invalid username or password' });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ message: 'Server error' });
+  }
+})
 
-  const newUser = new User({username});
+//SIGNUP API 
+router.post('/signup', async (req, res) => {
+  const { username, password, email  } = req.body;
+  console.log(req.body);
 
-  newUser.save()
+  //find for duplicate user 
+  const user = await User.findOne({username});
+  if(user) {
+    res.status(401).send({ message: 'Username not available!!' });
+    return;
+  }
+
+  //find for duplicate email 
+  const mail = await User.findOne({email});
+  if(mail) {
+    res.status(401).send({ message: 'Email not available!!' });
+    return;
+  }
+
+  try {
+    const newUser = new User({ username, password, email});
+    console.log("User added successfully");
+
+    newUser.save()
     .then(() => res.json('User added!'))
     .catch(err => res.status(400).json('Error: ' + err));
-});
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ message: 'Server error' });
+  }
+})
+
 
 module.exports = router; 
