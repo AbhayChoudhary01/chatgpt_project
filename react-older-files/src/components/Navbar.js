81979774848080611f2c58507bf62258d41f526a
Abhay Choudhary <abhaychoudhary804@gmail.com>
2023-04-02 17:46:40 -0400

    integrated NextJS

---
+++ react-older-files/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+
+
+export default function Navbar(){
+    return(
+        <nav className="header">
+            <img src = "images/Animation.gif" className = "navbar--logo" />
+            <h2 className="navbar--title">VOICE GPT</h2>
+        </nav>
+    )
+}
+
